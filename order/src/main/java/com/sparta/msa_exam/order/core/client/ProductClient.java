4e877bd29348b6cca3fa4404eacd30108b812fab
Bae Ji Won <qowhxk@naver.com>
2024-08-14 01:52:49 +0900

    FeignClient를 통해 Product 모듈과 통신

--- order/src/main/java/com/sparta/msa_exam/order/core/client/ProductClient.java
+++ order/src/main/java/com/sparta/msa_exam/order/core/client/ProductClient.java
@@ -5,14 +5,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+
 @FeignClient(name = "product-service")
 public interface ProductClient {
 
-    // Product 어플리케이션을 통해 상품 조회
-    @GetMapping("/products/{id}")
-    ProductResponseDto getProduct(@PathVariable("id") Long id);
+    // Product 어플리케이션을 통해 상품 모두 조회
+    @GetMapping("/products")
+    List<ProductResponseDto> getProduct();
 
-    // 수량 줄이기
-    @GetMapping("/products/{id}/reduceQuantity")
-    void reduceProductQuantity(@PathVariable("id") Long id, @RequestParam("quantity") int quantity);
 }
