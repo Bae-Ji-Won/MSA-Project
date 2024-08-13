51481a4f2bf24e9cf1af2f39faeb74a9a63fd48c
Bae Ji Won <qowhxk@naver.com>
2024-08-14 01:50:40 +0900

    GateWay Mono Filter

--- gateway/src/main/java/com/sparta/msa_exam/gateway/LocalJwtAuthenticationFilter.java
+++ gateway/src/main/java/com/sparta/msa_exam/gateway/LocalJwtAuthenticationFilter.java
@@ -27,7 +27,7 @@ public class LocalJwtAuthenticationFilter implements GlobalFilter {
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         String path = exchange.getRequest().getURI().getPath();
         if (path.equals("/auth/signIn") || path.equals("/auth/signUp")) {
-            return chain.filter(exchange);  // /signIn 경로는 필터를 적용하지 않음
+            return chain.filter(exchange);  // /signIn,signUp 경로는 필터를 적용하지 않음
         }
 
         String token = extractToken(exchange);
